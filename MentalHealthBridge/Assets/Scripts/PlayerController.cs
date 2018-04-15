9051af12114f5bee442c52d13c031255e8a1c074
Austin Leskin <ALeskin@student.fullsail.edu>
2018-04-14 20:49:40 -0400

    some head bobary

--- MentalHealthBridge/Assets/Scripts/PlayerController.cs
+++ MentalHealthBridge/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,19 @@ public class PlayerController : MonoBehaviour
     public bool beach;
     public AnxietyController anxiety;
 
+    Vector3 velo;
+
     float horizontalRot = 0;
     float verticalRot = 0;
+    float zRot = 0;
+
     float upDownRange = 45.0f;
     float speed = 250;
     float cameraSpeed = 5;
 
     float breathTime = 1;
+    float walkTime = 1;
+
 
     CharacterController characterController;
     public GameObject playerBase;
@@ -27,20 +33,38 @@ public class PlayerController : MonoBehaviour
 
 	void Update ()
     {
-        Vector3 velo = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        velo.Normalize();
-
         if (!breathe)
-            velo = ((transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"))) * speed;
+            velo = ((transform.right * Input.GetAxis("HorizontalButton")) + (transform.forward * Input.GetAxis("VerticalButton"))) * speed;
         velo.y = 0;
 
         //verticalVelocity += Physics.gravity.y * Time.deltaTime;
         //velo.y = verticalVelocity;
 
+        if (Input.GetButton("VerticalButton") )
+        {
+            Debug.Log("VericalButton");
+            if (walkTime > 0)
+                zRot += (Time.deltaTime * 5);
+            else if (walkTime >= -1)
+                zRot -= (Time.deltaTime * 5);
+
+            walkTime -= (Time.deltaTime);
+
+            if (walkTime < -1)
+                walkTime = 1;
+        }
+        else if(zRot != 0)
+        {
+            if (zRot < 0)
+                zRot += (Time.deltaTime * 5);
+            else if (zRot > 0)
+                zRot -= (Time.deltaTime * 5);
+        }
+
         horizontalRot += Input.GetAxis("Mouse X") * cameraSpeed;
         verticalRot -= Input.GetAxis("Mouse Y") * cameraSpeed;
         verticalRot = Mathf.Clamp(verticalRot, -upDownRange, upDownRange - 20);
-        playerBase.transform.localRotation = Quaternion.Euler(verticalRot, horizontalRot, 0);
+        playerBase.transform.localRotation = Quaternion.Euler(verticalRot, horizontalRot, zRot);
 
         if (breathe)
         {
